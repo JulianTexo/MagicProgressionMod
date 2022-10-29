a74f5d7cbf739abf0df1036ba583dbdb2f2ac430
Julian Texo <JulianTexo@gmail.com>
2022-10-29 16:22:32 +0200

    added mana_crystal_ore but drops dont work yet

--- src/main/java/net/juliantexo/magicprogression/block/ModBlocks.java
+++ src/main/java/net/juliantexo/magicprogression/block/ModBlocks.java
@@ -5,10 +5,12 @@ import net.juliantexo.magicprogression.block.custom.ManaFurnaceBlock;
 import net.juliantexo.magicprogression.block.custom.ManaInfusingStationBlock;
 import net.juliantexo.magicprogression.item.ModItemGroup;
 import net.juliantexo.magicprogression.item.ModItems;
+import net.minecraft.util.valueproviders.UniformInt;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.DropExperienceBlock;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.Material;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -22,12 +24,21 @@ public class ModBlocks {
     public static final DeferredRegister<Block> BLOCKS =
             DeferredRegister.create(ForgeRegistries.BLOCKS, MagicProgression.MOD_ID);
 
+    //Machines
     public static final RegistryObject<Block> MANA_FURNACE = registerBlock("mana_furnace",
             () -> new ManaFurnaceBlock(BlockBehaviour.Properties.of(Material.STONE)
                     .lightLevel(state -> state.getValue(ManaFurnaceBlock.LIT) ? 7 : 0)), ModItemGroup.TAB);
 
     public static final RegistryObject<Block> MANA_INFUSING_STATION = registerBlock("mana_infusing_station",
             () -> new ManaInfusingStationBlock(BlockBehaviour.Properties.of(Material.STONE)), ModItemGroup.TAB);
+
+    //Ores
+    public static final RegistryObject<Block> MANA_CRYSTAL_ORE = registerBlock("mana_crystal_ore",
+            () -> new DropExperienceBlock(BlockBehaviour.Properties.of(Material.STONE)
+                    .strength(4f).requiresCorrectToolForDrops(), UniformInt.of(3, 7)),
+            ModItemGroup.TAB);
+
+    //Blocks
 
     private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block, CreativeModeTab tab){
         RegistryObject<T> toReturn = BLOCKS.register(name, block);
